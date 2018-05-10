4b114e1c245098c9f33a59e559a37a490c1d9371
Nicholas Jones <nicholasaaronjones@gmail.com>
2018-05-10 10:33:51 -0500

    bootstrap/popper/layout

---
+++ src/app/customheader/customheader.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomheaderComponent } from './customheader.component';
+
+describe('CustomheaderComponent', () => {
+  let component: CustomheaderComponent;
+  let fixture: ComponentFixture<CustomheaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CustomheaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomheaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/customheader/customheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customheader',
+  templateUrl: './customheader.component.html',
+  styleUrls: ['./customheader.component.css']
+})
+export class CustomheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
